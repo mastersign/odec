a0639cbec5fd1fe207d48138012066f7cbd62af4
mastersign <dev@mastersign.de>
2013-05-08 09:54:36 +0200

    initialize C# project 'odec' as class library and command line interface

---
+++ src/clr/odec/cli/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace de.mastersign.odec.cli
+{
+    /// <summary>
+    /// The main module for the command line interface of the ODEC library.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// The entry point for the command line interface.
+        /// </summary>
+        /// <param name="args">The command line arguments.</param>
+        static void Main(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
